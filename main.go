905f237e8cfe2130499db2817ea52c4a1cc687bf
izumin5210 <m@izum.in>
2018-08-10 11:54:04 +0900

    Impl Mask()

--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
+import "encoding/json"
+
+var (
+	keywordSet = map[string]struct{}{
+		"password":              struct{}{},
+		"password_confirmation": struct{}{},
+		"token":                 struct{}{},
+	}
+)
+
 func Mask(data []byte) ([]byte, error) {
-	return nil
+	var v interface{}
+
+	err := json.Unmarshal(data, &v)
+	if err != nil {
+		return nil, err
+	}
+
+	out, err := json.Marshal(mask(v))
+	if err != nil {
+		return nil, err
+	}
+
+	return out, nil
+}
+
+func mask(in interface{}) interface{} {
+	switch in := in.(type) {
+	case map[string]interface{}:
+		for k, v := range in {
+			if _, ok := keywordSet[k]; ok {
+				in[k] = "[FILTERED]"
+			} else {
+				in[k] = mask(v)
+			}
+		}
+		return in
+	case []interface{}:
+		masked := make([]interface{}, 0, len(in))
+		for _, v := range in {
+			masked = append(masked, mask(v))
+		}
+		return masked
+	}
+	return in
 }
